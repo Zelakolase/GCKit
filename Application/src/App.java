2e623acd8862c40960f4e52cd32d1e113cc74072
Morad <460583232@sharkia1.moe.edu.eg>
2023-07-03 02:22:50 +0300

    Graph Example;

--- Application/src/App.java
+++ Application/src/App.java
@@ -1,6 +1,10 @@
+import classlib.Graph;
+
 public class App {
     public static void main(String[] args) throws Exception {
         /* Declare a Graph */
+        Graph G = new Graph();
+        G.importTSV("../data/GO:0008217.tsv");
         /* Cluster it */
         /* Evaluate it */
     }
