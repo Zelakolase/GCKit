eb0bbcc9a5a3f22265200c43e9bbed869070761b
Morad <460583232@sharkia1.moe.edu.eg>
2023-07-03 05:17:33 +0300

    README change; Readability; DeltaStrength init

--- Application/src/App.java
+++ Application/src/App.java
@@ -4,8 +4,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         /* Declare a Graph */
         Graph G = new Graph();
+        /*
+        As mentioned in the original README.md, you have to execute the application while you are in
+        'Application/src', because in the next line, we move to the parent directory and access 'data'
+        folder, then the file.
+        */
         G.importTSV("../data/GO:0008217.tsv");
+
         /* Cluster it */
+        
         /* Evaluate it */
     }
 }
--- Application/src/Cyclops/DeltaStrength.java
+++ Application/src/Cyclops/DeltaStrength.java
@@ -1,5 +1,13 @@
 package Cyclops;
 
+import java.util.HashMap;
+
+import classlib.Graph;
+
 public class DeltaStrength {
-    
+    public static HashMap<String, Double> compute(Graph G) {
+        HashMap<String, Double> output = new HashMap<>();
+        
+        return output;
+    }
 }
